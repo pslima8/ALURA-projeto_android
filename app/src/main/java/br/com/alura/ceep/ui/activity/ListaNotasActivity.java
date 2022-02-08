7b17b29372fbf2dd8adfdddcddb482f1b7bc20f5
Rodrigo Engster de Lima <pslima8@hotmail.com>
2022-02-08 16:07:02 -0300

    Entrega da Funcionalidade#1 Projeto Android

---
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,229 @@
+package br.com.alura.ceep.ui.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.List;
+
+import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.recyclerview.adapter.ListaNotasAdapter;
+import br.com.alura.ceep.ui.recyclerview.adapter.listener.OnItemClickListener;
+import br.com.alura.ceep.ui.recyclerview.helper.callback.NotaItemTouchHelperCallback;
+
+import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CHAVE_NOTA;
+import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CHAVE_POSICAO;
+import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CODIGO_REQUISICAO_ALTERA_NOTA;
+import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CODIGO_REQUISICAO_INSERE_NOTA;
+import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.POSICAO_INVALIDA;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+
+    public static final String TITULO_APPBAR = "Notas";
+    private ListaNotasAdapter adapter;
+    private Menu menu;
+    private int layout;
+    private SharedPreferences layoutArmasenado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+
+        setTitle(TITULO_APPBAR);
+
+        List<Nota> todasNotas = pegaTodasNotas();
+        configuraRecyclerView(todasNotas);
+        configuraBotaoInsereNota();
+    }
+
+    //início das configurações do menu de seleção de layout
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_lista_notas, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        LinearLayoutManager linearLayout = new LinearLayoutManager(this);
+        StaggeredGridLayoutManager gridLayout = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+
+        RecyclerView listaNotasRecyclerview = findViewById(R.id.lista_notas_recyclerview);
+
+        //lê dados armasenados de layout
+        layoutArmasenado = this.getSharedPreferences("layout", Context.MODE_PRIVATE);
+        this.layout = layoutArmasenado.getInt("layout", 0);
+
+        //testa layout armasenado e aplica ao menu e ao Recyclerview
+        if (this.layout == 0){
+            //exibe layou linear
+            this.menu.getItem(0).setIcon(getResources().getDrawable(R.drawable.menu_grid));
+            this.menu.getItem(0).setTitle(R.string.grid);
+            listaNotasRecyclerview.setLayoutManager(linearLayout);
+        } else {
+            //exibe layout grid
+            this.menu.getItem(0).setIcon(getResources().getDrawable(R.drawable.menu_linear));
+            this.menu.getItem(0).setTitle(R.string.linear);
+            listaNotasRecyclerview.setLayoutManager(gridLayout);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //testa o item do menu clicado
+        int id = item.getItemId();
+        //alterna a opção de exibição e salva a opção aplicada
+        if (id == R.id.layout){
+            if (this.layout == 0){
+                this.layout = 1;
+            } else {
+                this.layout = 0;
+            }
+            layoutArmasenado.edit().putInt("layout", layout).apply();
+            onPrepareOptionsMenu(menu);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //final das configurações do menu de seleção de layout
+
+    private void configuraBotaoInsereNota() {
+        TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        botaoInsereNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vaiParaFormularioNotaActivityInsere();
+            }
+        });
+    }
+
+    private void vaiParaFormularioNotaActivityInsere() {
+        Intent iniciaFormularioNota =
+                new Intent(ListaNotasActivity.this,
+                        FormularioNotaActivity.class);
+        startActivityForResult(iniciaFormularioNota,
+                CODIGO_REQUISICAO_INSERE_NOTA);
+    }
+
+    private List<Nota> pegaTodasNotas() {
+        NotaDAO dao = new NotaDAO();
+        return dao.todos();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (ehResultadoInsereNota(requestCode, data)) {
+
+            if (resultadoOk(resultCode)) {
+                Nota notaRecebida = (Nota) data.getSerializableExtra(CHAVE_NOTA);
+                adiciona(notaRecebida);
+            }
+
+        }
+
+        if (ehResultadoAlteraNota(requestCode, data)) {
+            if (resultadoOk(resultCode)) {
+                Nota notaRecebida = (Nota) data.getSerializableExtra(CHAVE_NOTA);
+                int posicaoRecebida = data.getIntExtra(CHAVE_POSICAO, POSICAO_INVALIDA);
+                if (ehPosicaoValida(posicaoRecebida)) {
+                    altera(notaRecebida, posicaoRecebida);
+                }
+            }
+        }
+    }
+
+    private void altera(Nota nota, int posicao) {
+        new NotaDAO().altera(posicao, nota);
+        adapter.altera(posicao, nota);
+    }
+
+    private boolean ehPosicaoValida(int posicaoRecebida) {
+        return posicaoRecebida > POSICAO_INVALIDA;
+    }
+
+    private boolean ehResultadoAlteraNota(int requestCode, Intent data) {
+        return ehCodigoRequisicaoAlteraNota(requestCode) &&
+                temNota(data);
+    }
+
+    private boolean ehCodigoRequisicaoAlteraNota(int requestCode) {
+        return requestCode == CODIGO_REQUISICAO_ALTERA_NOTA;
+    }
+
+    private void adiciona(Nota nota) {
+        new NotaDAO().insere(nota);
+        adapter.adiciona(nota);
+    }
+
+    private boolean ehResultadoInsereNota(int requestCode, Intent data) {
+        return ehCodigoRequisicaoInsereNota(requestCode) &&
+                temNota(data);
+    }
+
+    private boolean temNota(Intent data) {
+        return data != null && data.hasExtra(CHAVE_NOTA);
+    }
+
+    private boolean resultadoOk(int resultCode) {
+        return resultCode == Activity.RESULT_OK;
+    }
+
+    private boolean ehCodigoRequisicaoInsereNota(int requestCode) {
+        return requestCode == CODIGO_REQUISICAO_INSERE_NOTA;
+    }
+
+    private void configuraRecyclerView(List<Nota> todasNotas) {
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
+        configuraAdapter(todasNotas, listaNotas);
+        configuraItemTouchHelper(listaNotas);
+    }
+
+    private void configuraItemTouchHelper(RecyclerView listaNotas) {
+        ItemTouchHelper itemTouchHelper =
+                new ItemTouchHelper(new NotaItemTouchHelperCallback(adapter));
+        itemTouchHelper.attachToRecyclerView(listaNotas);
+    }
+
+    private void configuraAdapter(List<Nota> todasNotas, RecyclerView listaNotas) {
+        adapter = new ListaNotasAdapter(this, todasNotas);
+        listaNotas.setAdapter(adapter);
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(Nota nota, int posicao) {
+                vaiParaFormularioNotaActivityAltera(nota, posicao);
+            }
+        });
+    }
+
+    private void vaiParaFormularioNotaActivityAltera(Nota nota, int posicao) {
+        Intent abreFormularioComNota = new Intent(ListaNotasActivity.this,
+                FormularioNotaActivity.class);
+        abreFormularioComNota.putExtra(CHAVE_NOTA, nota);
+        abreFormularioComNota.putExtra(CHAVE_POSICAO, posicao);
+        startActivityForResult(abreFormularioComNota, CODIGO_REQUISICAO_ALTERA_NOTA);
+    }
+
+}
